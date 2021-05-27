7354354c2d674ff065681b0a1899bda2993ba467
Guilherme <guilhermegufes@gmail.com>
2021-05-27 13:41:58 -0300

    create userService

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,36 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User  from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if(checkUserExists) {
+      throw new Error('Email address already used.');
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    const user = usersRepository.create({
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
