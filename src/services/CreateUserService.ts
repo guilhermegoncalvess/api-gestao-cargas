bbc52401991533aa401563cb44520a60a4da387d
Guilherme <guilhermegufes@gmail.com>
2021-06-22 15:23:18 -0300

    update services

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -4,15 +4,25 @@ import { hash } from 'bcryptjs';
 import AppError from '../errors/AppError';
 
 import User  from '../models/User';
+import Company  from '../models/Company';
 
 interface Request {
+  company_id: string;
   email: string;
   password: string;
+  role: string;
 }
 
 class CreateUserService {
-  public async execute({ email, password }: Request): Promise<User> {
+  public async execute({ company_id, email, password, role }: Request): Promise<User> {
     const usersRepository = getRepository(User);
+    const companiesRepository = getRepository(Company);
+
+    const company = await companiesRepository.findOne(company_id)
+
+    if(!company) {
+      throw new AppError('Company does not exist.', 404);
+    }
 
     const checkUserExists = await usersRepository.findOne({
       where: { email },
@@ -25,8 +35,10 @@ class CreateUserService {
     const hashedPassword = await hash(password, 8)
 
     const user = usersRepository.create({
+      company_id,
       email,
       password: hashedPassword,
+      role,
     });
 
     await usersRepository.save(user);
