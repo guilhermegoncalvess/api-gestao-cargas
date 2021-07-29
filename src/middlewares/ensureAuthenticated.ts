bf79930afc863cf93ce5562a630b66087ee8665b
Guilherme <guilhermegufes@gmail.com>
2021-07-29 09:29:25 -0300

    update middleware to handle roles user

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,9 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
+import { getRepository } from 'typeorm';
 import { verify } from 'jsonwebtoken';
+
+import Role from '../models/Role';
 
 import AppError from '../errors/AppError';
 
 import authConfig from '../config/auth';
+import routes from '../routes';
+import User from '../models/User';
+
 
 interface TokenPayload {
   iat: number;
@@ -11,28 +17,66 @@ interface TokenPayload {
   sub: string;
 }
 
-export default function ensureAuthenticate(request: Request, response: Response, next: NextFunction): void {
+export default async function ensureAuthenticate(request: Request, response: Response, next: NextFunction): Promise<void> {
 
   const authHeader = request.headers.authorization;
+  const url = request.baseUrl;
+  const method = request.method;
+
 
   if(!authHeader){
     throw new AppError('JWT token is missing', 401);
   }
 
-  const [, token] = authHeader.split(' ');
-
   try {
-    const decoded = verify(token, authConfig.jwt.secret);
 
-    const { sub } = decoded as TokenPayload;
+      const usersRepository = getRepository(User);
+      const rolesRepository = getRepository(Role);
 
-    request.user = {
-      id: sub,
-    };
+      const [, token] = authHeader.split(' ');
+      const [, path] = url.split('/');
 
-    return next();
 
-  } catch {
-    throw new AppError('Invaild JWT token.', 401)
-  }
+      const decoded = verify(token, authConfig.jwt.secret);
+
+      const { sub } = decoded as TokenPayload;
+
+      //Separar em outro middlewares a partir daqui para lidar apenas com os roles do usuário
+      const user = await usersRepository.findOne(sub)
+
+      if(user) {
+        const role = await rolesRepository.findOne(user.role_id)
+
+        if(method === 'GET') {
+          if(!role?.permission[path].read) {
+            throw new AppError('Permission denied', 401)
+          }
+        }
+
+        if(method === 'POST') {
+          if(!role?.permission[path].create) {
+            throw new AppError('Permission denied', 401)
+          }
+        }
+
+        if(method === 'PUT') {
+          if(!role?.permission[path].update) {
+            throw new AppError('Permission denied', 401)
+          }
+        }
+
+        if(method === 'DELETE') {
+          if(!role?.permission[path].delete) {
+            throw new AppError('Permission denied', 401)
+          }
+        }
+        return next();
+
+      }
+
+    } catch(err){
+      throw new AppError('Invaild JWT token.', 401)
+    }
+
+
 }
