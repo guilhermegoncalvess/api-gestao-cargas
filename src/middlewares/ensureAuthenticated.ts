4f8faa05ae979ef86839b5eddef51e663c8e14ed
Guilherme <guilhermegufes@gmail.com>
2021-08-03 16:40:14 -0300

    update for ensure authenticate per role user

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -7,17 +7,19 @@ import Role from '../models/Role';
 import AppError from '../errors/AppError';
 
 import authConfig from '../config/auth';
-import routes from '../routes';
 import User from '../models/User';
 
-
-interface TokenPayload {
-  iat: number;
-  exp: number;
-  sub: string;
-}
+import verifyToken from '../utils/VerifyToken'
 
 export default async function ensureAuthenticate(request: Request, response: Response, next: NextFunction): Promise<void> {
+
+
+  const methodsPermissionsKeys: any  = {
+    GET: "read",
+    POST: "create",
+    PUT: "update",
+    DELETE: "delete",
+  }
 
   const authHeader = request.headers.authorization;
   const url = request.baseUrl;
@@ -33,49 +35,40 @@ export default async function ensureAuthenticate(request: Request, response: Res
       const usersRepository = getRepository(User);
       const rolesRepository = getRepository(Role);
 
-      const [, token] = authHeader.split(' ');
+
       const [, path] = url.split('/');
 
-
-      const decoded = verify(token, authConfig.jwt.secret);
-
-      const { sub } = decoded as TokenPayload;
+      const { user_id, company_id } = verifyToken( authHeader, authConfig.jwt.secret);
 
       //Separar em outro middlewares a partir daqui para lidar apenas com os roles do usuário
-      const user = await usersRepository.findOne(sub)
+      const user = await usersRepository.findOne({id: user_id, company_id})
 
       if(user) {
-        const role = await rolesRepository.findOne(user.role_id)
+        const role = await rolesRepository.findOne({id:user.role_id})
 
-        if(method === 'GET') {
-          if(!role?.permission[path].read) {
-            throw new AppError('Permission denied', 401)
-          }
-        }
+        if(role) {
+          if(!(role.name === 'super')) {
+            const keysRole = Object.keys(role.permission);
 
-        if(method === 'POST') {
-          if(!role?.permission[path].create) {
-            throw new AppError('Permission denied', 401)
+            if(!keysRole.find( entity => entity === path.toLowerCase())) {
+              throw new AppError('Permission denied', 401);
+            }
+            if(!role.permission[path][methodsPermissionsKeys[method]]){
+              throw new AppError('Permission denied', 401);
+            }
+            return next();
           }
+          return next();
         }
-
-        if(method === 'PUT') {
-          if(!role?.permission[path].update) {
-            throw new AppError('Permission denied', 401)
-          }
-        }
-
-        if(method === 'DELETE') {
-          if(!role?.permission[path].delete) {
-            throw new AppError('Permission denied', 401)
-          }
-        }
-        return next();
 
       }
 
     } catch(err){
-      throw new AppError('Invaild JWT token.', 401)
+      if( err instanceof AppError) {
+        throw new AppError(err.message, 401);
+      } else {
+        throw new AppError('Invalid JWT token.', 401);
+      }
     }
 
 
