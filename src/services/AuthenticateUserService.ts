e68290dc83114c47f00744713a59da81e4d9c6e4
Guilherme <guilhermegufes@gmail.com>
2021-08-03 16:49:32 -0300

    add atribute of company_id in paylod token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -37,7 +37,10 @@ class AuthenticateUserService {
 
     const { secret, expiresIn } = authConfig.jwt;
 
-    const token = sign({}, secret, {
+    const token = sign({
+      company_id: user.company_id,
+      user_id: user.id,
+    }, secret, {
       subject: user.id,
       expiresIn,
     });
